7608dde28829b059e2890edfaf3aa0c882389d0b
abyss616 <teodors.berzins@gmail.com>
2026-03-29 08:40:37 +0300

    fixed  coordinates

--- src/ScreenshotScraper.Extraction/HandHistory/SixMaxTableVisionLayout.cs
+++ src/ScreenshotScraper.Extraction/HandHistory/SixMaxTableVisionLayout.cs
@@ -6,7 +6,6 @@ public sealed class SixMaxTableVisionLayout
 {
     public const int ReferenceWidth = 1020;
     public const int ReferenceHeight = 717;
-
     private readonly IReadOnlyDictionary<int, SeatVisionRoi> _seatRois;
 
     public SixMaxTableVisionLayout()
@@ -15,12 +14,12 @@ public sealed class SixMaxTableVisionLayout
         // Per-seat offsets are intentional because each seat panel has slightly different perspective and HUD overlap.
         _seatRois = new Dictionary<int, SeatVisionRoi>
         {
-            [1] = new(1, new Rectangle(369, 453, 72, 72), new Rectangle(425, 505, 185, 104), NameRoi: new Rectangle(432, 565, 165, 30), StackRoi: new Rectangle(432, 590, 165, 27), new Rectangle(432, 583, 165, 24)),
-            [2] = new(2, new Rectangle(297, 432, 104, 80), new Rectangle(124, 500, 192, 104), NameRoi: new Rectangle(134, 510, 170, 30), StackRoi: new Rectangle(130, 535, 170, 27), new Rectangle(134, 579, 170, 24)),
-            [3] = new(3, new Rectangle(158, 162, 120, 94), new Rectangle(56, 188, 182, 104), NameRoi: new Rectangle(60, 212, 165, 30), StackRoi: new Rectangle(50, 233, 165, 27), new Rectangle(66, 268, 165, 24)),
-            [4] = new(4, new Rectangle(428, 142, 120, 94), new Rectangle(414, 76, 192, 104), NameRoi: new Rectangle(426, 125, 170, 30), StackRoi: new Rectangle(426, 145, 170, 27), new Rectangle(426, 157, 170, 24)),
-            [5] = new(5, new Rectangle(818, 162, 120, 94), new Rectangle(806, 188, 188, 104), NameRoi: new Rectangle(820, 210, 164, 30), StackRoi: new Rectangle(820, 232, 164, 27), new Rectangle(820, 268, 164, 24)),
-            [6] = new(6, new Rectangle(650, 440, 120, 94), new Rectangle(730, 500, 196, 104), NameRoi: new Rectangle(748, 510, 164, 30), StackRoi: new Rectangle(735, 535, 164, 27), new Rectangle(748, 579, 164, 24))
+            [1] = new(1, new Rectangle(369, 453, 72, 72), new Rectangle(425, 505, 185, 104), NameRoi: new Rectangle(432, 565, 165, 30), StackRoi: new Rectangle(432, 590, 165, 27), BetRoi: new Rectangle(500, 459, 120, 25)),
+            [2] = new(2, new Rectangle(297, 432, 104, 80), new Rectangle(124, 500, 192, 104), NameRoi: new Rectangle(134, 510, 170, 30), StackRoi: new Rectangle(130, 535, 170, 27), BetRoi: new Rectangle(269, 454, 120, 25)),
+            [3] = new(3, new Rectangle(158, 162, 120, 94), new Rectangle(56, 188, 182, 104), NameRoi: new Rectangle(60, 212, 165, 30), StackRoi: new Rectangle(50, 233, 165, 27), BetRoi: new Rectangle(193, 269, 120, 25)),
+            [4] = new(4, new Rectangle(428, 142, 120, 94), new Rectangle(414, 76, 192, 104), NameRoi: new Rectangle(426, 125, 170, 30), StackRoi: new Rectangle(426, 145, 170, 27), BetRoi: new Rectangle(421, 212, 120, 25)),
+            [5] = new(5, new Rectangle(818, 162, 120, 94), new Rectangle(806, 188, 188, 104), NameRoi: new Rectangle(820, 210, 164, 30), StackRoi: new Rectangle(820, 232, 164, 27), BetRoi: new Rectangle(820, 268, 164, 24)),
+            [6] = new(6, new Rectangle(650, 440, 120, 94), new Rectangle(730, 500, 196, 104), NameRoi: new Rectangle(748, 510, 164, 30), StackRoi: new Rectangle(735, 535, 164, 27), BetRoi: new Rectangle(748, 579, 164, 24))
         };
     }
 
